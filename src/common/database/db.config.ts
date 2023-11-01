7e0220fc0339ecdb4ab91c7cac0df63db12666c7
mrpaflex <ukosaviour21@gmail.com>
2023-11-01 20:23:35 +0100

    config db

--- src/common/database/db.config.ts
+++ src/common/database/db.config.ts
@@ -5,11 +5,11 @@ require('dotenv').config();
 
 export const db: PostgresConnectionOptions = {
     type: 'postgres',
-    host: process.env.PGHOST,
-    password: process.env.PGPASSWORD,
-    port: parseInt(process.env.PGPORT),
-    database: process.env.PGDATABASE,
-    username: process.env.PGUSERNAME,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
     entities: ["dist/**/*.entity{.ts,.js}"],
     synchronize: true
     }
