1775f97ca7fbe1b68f4768a03de9b4f7e7984c75
mrpaflex <ukosaviour21@gmail.com>
2023-11-01 20:13:39 +0100

    configuration set

--- src/common/database/db.config.ts
+++ src/common/database/db.config.ts
@@ -1,14 +1,15 @@
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 
+
 require('dotenv').config();
 
 export const db: PostgresConnectionOptions = {
     type: 'postgres',
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT),
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
+    host: process.env.PGHOST,
+    password: process.env.PGPASSWORD,
+    port: parseInt(process.env.PGPORT),
+    database: process.env.PGDATABASE,
+    username: process.env.PGUSERNAME,
     entities: ["dist/**/*.entity{.ts,.js}"],
     synchronize: true
     }
