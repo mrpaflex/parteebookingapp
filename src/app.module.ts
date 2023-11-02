01e5c9bcbe6cc9eaadce220b6beb80f447006388
mrpaflex <ukosaviour21@gmail.com>
2023-11-02 11:31:17 +0100

    uri added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { db } from './common/database/db.config';
+//import { db } from './common/database/db.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { VendorModule } from './vendor/vendor.module';
 import { PlannerModule } from './planner/planner.module';
 import { ProductModule } from './product/product.module';
 import { MailModule } from './mail/mail.module';
+import { db } from './common/database/db.config';
+//import { db } from './common/database/db.config';
 
 require('dotenv').config();
 
@@ -23,7 +25,18 @@ require('dotenv').config();
       autoSchemaFile: true,
       installSubscriptionHandlers: true
     }),
-    TypeOrmModule.forRoot(db),  
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: db.url,
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+      ssl: true, // Enable SSL/TLS
+      extra: {
+        ssl: {
+          rejectUnauthorized: false, // You may need to set this to false if using self-signed certificates
+        },
+      },
+    }),  
     UserModule, AuthModule, VendorModule, PlannerModule, ProductModule, MailModule
   ],
 })
--- src/common/database/db.config.ts
+++ src/common/database/db.config.ts
@@ -1,15 +1,21 @@
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+//import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { ConfigService } from '@nestjs/config';
+import {config} from 'dotenv'
 
-
-require('dotenv').config();
-
-export const db: PostgresConnectionOptions = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT),
-    database: process.env.DB_DATABASE,
-    username: process.env.DB_USERNAME,
-    entities: ["dist/**/*.entity{.ts,.js}"],
-    synchronize: true
+config()
+export const db= {
+    url: process.env.DATABASE_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    TOKEN_KEY: process.env.TOKEN_KEY,
+    TOKEN_EXPIRY: process.env.TOKEN_EXPIRY
     }
+
+    // type: 'postgres',
+    // PostgresConnectionOptions
+    // host: process.env.DB_HOST,
+    // password: process.env.DB_PASSWORD,
+    // port: parseInt(process.env.DB_PORT),
+    // database: process.env.DB_DATABASE,
+    // username: process.env.DB_USERNAME,
+
+  
